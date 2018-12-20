bcc136ebc91d99d0b693a73e1a0da06a23e5e9bc
Aphyr <aphyr@aphyr.com>
2018-12-20 09:43:28 -0500

    Tools.logging 0.4.1

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/aphyr/tea-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.logging "0.4.0"]]
+  :dependencies [[org.clojure/tools.logging "0.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
